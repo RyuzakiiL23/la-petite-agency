ec24bdd347579ca1407c071c2752f8d05793d142
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2025-08-16 12:49:27 +0100

    spline

---
+++ la-petite-agency/app/3d/page.tsx
@@ -0,0 +1,10 @@
+import SplineFile from '@/components/SplineFile'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='h-fit w-full'>
+        <SplineFile/>
+    </div>
+  )
+}
---
+++ la-petite-agency/components/SplineFile.tsx
@@ -0,0 +1,12 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function SplineFile() {
+  return (
+    <main>
+      <Spline
+                      scene="https://prod.spline.design/AP-CcebWqcPv48Sx/scene.splinecode" 
+
+      />
+    </main>
+  );
+}
