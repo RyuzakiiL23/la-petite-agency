96d370fe097722482d05940d43ac760debd58cd7
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2025-08-16 13:14:56 +0100

    spline almost responsive

--- la-petite-agency/app/3d/page.tsx
+++ la-petite-agency/app/3d/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='h-fit w-full'>
+    <div className='h-fit w-full relative'>
+        <div className='absolute h-10 w-40 bg-[#0b0913] rounded-full bottom-4 text-white border-white border right-4'>
+            <div className='flex justify-center items-center h-full'>
+                Ryu
+            </div>
+        </div>
         <SplineFile/>
     </div>
   )
