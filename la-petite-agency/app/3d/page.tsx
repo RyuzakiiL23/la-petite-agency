b79080c95826ece4c07559045612bb976add57c2
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2025-09-18 02:33:58 +0100

    strategy table

--- la-petite-agency/app/3d/page.tsx
+++ la-petite-agency/app/3d/page.tsx
@@ -2,14 +2,16 @@ import SplineFile from '@/components/SplineFile'
 import React from 'react'
 
 export default function page() {
-  return (
-    <div className='h-fit w-full relative'>
-        <div className='absolute h-10 w-40 bg-[#0b0913] rounded-full bottom-4 text-white border-white border right-4'>
+    return (
+        <div className='h-fit w-full relative overflow-hidden rotate'>
+            {/* <div className='absolute h-10 w-40 bg-[#0b0913] rounded-full bottom-4 text-white border-white border right-4'>
             <div className='flex justify-center items-center h-full'>
                 Ryu
             </div>
+        </div> */}
+            <div className='w-[125[vw] h-[60vw] rotating mx-[-40vw] '>
+                <SplineFile />
+            </div>
         </div>
-        <SplineFile/>
-    </div>
-  )
+    )
 }
